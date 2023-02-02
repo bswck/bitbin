e2b97d70c5a3d7145ef6b98b3b312dbc95f39fe6
bswck <bswck.dev@gmail.com>
2023-02-02 18:20:53 +0100

    +

--- constance/__init__.py
+++ constance/__init__.py
@@ -1,16 +1,16 @@
 from constance._constants import DEFAULT_ENCODING
 
-from constance.classes import *
+from constance.impl import *
 from constance.core import *
 from constance.config import *
 from constance.util import *
 
-from constance import classes, config, core, util
+from constance import impl, config, core, util
 
 from construct import this
 
 __all__ = (
-    'classes',
+    'impl.py',
     'config',
     'core',
     'util',
