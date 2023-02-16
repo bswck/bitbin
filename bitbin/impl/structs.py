f6dd7a339adbc3d24d134e5d2cf7aa30cd44d6cf
bswck <bswck.dev@gmail.com>
2023-02-16 22:54:23 +0100

    +

---
+++ bitbin/impl/structs.py
@@ -0,0 +1,50 @@
+import functools
+import construct as _lib
+from bitbin import core
+
+
+__all__ = (
+    'Struct',
+    'BitStruct',
+    'LazyStruct',
+    'AlignedStruct',
+)
+
+
+class Struct(core.ModelDataclass, _bitbin=True):
+    _impl = _lib.Struct
+
+
+class BitStruct(core.ModelDataclass, _bitbin=True):
+    _impl = _lib.BitStruct
+
+
+class LazyStruct(core.ModelDataclass, _bitbin=True):
+    _impl = _lib.LazyStruct
+
+
+class AlignedStruct(core.ModelDataclass, _bitbin=True):
+    _impl = _lib.AlignedStruct
+
+    @classmethod
+    def _modulus(cls, context):
+        raise NotImplementedError
+
+    def __init_subclass__(
+            cls, _bitbin=False,
+            stack_offset=1,
+            annotation_manager=None
+    ):
+        if _bitbin:
+            return
+        super().__init_subclass__(
+            stack_offset=stack_offset+1,
+            annotation_manager=annotation_manager
+        )
+        cls._impl = functools.partial(cls._impl, cls._modulus)
+
+
+class Union(core.ModelDataclass):
+    """Port to construct.Union"""
+
+    _impl = _lib.Union  # (parsefrom, *subcons, **subconskw)
